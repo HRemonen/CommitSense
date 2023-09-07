be35b66749f0de36f0e1c2329e6a3068d614f116
Henri Remonen <henri@remonen.fi>
2023-09-07 22:04:16 +0300

    feat(CoAuthor): Initial functionality for the CoAuthor adding
    refs: #1

--- pkg/author/author.go
+++ pkg/author/author.go
@@ -3,27 +3,34 @@ package author
 import (
 	"fmt"
 	"os/exec"
+	"regexp"
 	"strings"
 )
 
+func extractNameAndEmail(line string) []string {
+	pattern := `(\w[\w\s]+)\s+<([^>]+)>`
+	re := regexp.MustCompile(pattern)
+	return re.FindStringSubmatch(line)
+}
+
 func GetSuggestedCoAuthors() ([]string, error) {
 	// Use the `git rev-list` command to obtain a list of authors who have made commits in the Git repository.
-	cmd := exec.Command("git", "log", "--format='%aN <%aE>'", "--all", "--no-merges", "|", "sort", "--unique")
+	revlist := "git log --pretty='%an <%ae>' | sort -u"
+	cmd := exec.Command("bash", "-c", revlist)
 	output, err := cmd.CombinedOutput()
+
 	if err != nil {
 		return nil, err
 	}
 
-	// Parse the output to extract author names and email addresses.
-	lines := strings.Split(string(output), "\n")
+	authorString := strings.TrimSpace(string(output))
 
-	fmt.Println(lines)
-	var suggestedCoAuthors []string
-	for _, line := range lines {
-		if strings.HasPrefix(line, "'") && strings.HasSuffix(line, "'") {
-			suggestedCoAuthors = append(suggestedCoAuthors, strings.Trim(line, "'"))
-		}
-	}
+	strings.Replace(authorString, `\n`, "\n", -1)
+
+	// Parse the output to extract author names and email addresses.
+	suggestedCoAuthors := strings.Split(authorString, "\n")
+
+	fmt.Println("suggested authors", suggestedCoAuthors)
 
 	return suggestedCoAuthors, nil
 }
