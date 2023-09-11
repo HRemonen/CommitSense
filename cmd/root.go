22271c5921f1cabec49c4116fe4311d69d6f04d2
Henri Remonen <henri@remonen.fi>
2023-09-11 20:03:22 +0300

    fix: Update flag messages and command help texts

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,7 @@
 /*
 Package cmd provides the main entry point for the CommitSense command-line tool.
 
-CommitSense is a command-line tool designed to improve commit messages and manage GitHub issues. This package contains the root command and initializes the application. It also defines global flags and configuration settings.
+CommitSense is a command-line tool designed to improve commit messages. This package contains the root command and initializes the application. It also defines global flags and configuration settings.
 
 For more information on how to use CommitSense, run 'commitsense help'.
 
@@ -16,19 +16,19 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "commitsense",
-	Short: "A tool to improve commit messages and manage GitHub issues",
-	Long:  `CommitSense is a command-line tool that helps you follow best practices for commit messages and manage GitHub issues.`,
+	Short: "A tool to improve commit messages",
+	Long: `CommitSense is a command-line tool that simplifies Git 
+	version control by providing an interactive and standardized way to stage 
+	files and create commit messages following the Conventional Commits specification.
+	`,
+	TraverseChildren: true,
 	Run: func(cmd *cobra.Command, args []string) {
-		// This is the default action when the tool is run without a subcommand.
 		fmt.Println("Welcome to CommitSense!")
 	},
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
@@ -37,13 +37,5 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.commitsense.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
