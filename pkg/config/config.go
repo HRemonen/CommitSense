0a71d857efa6c20db4fe0d96b868d190810e6db9
Henri Remonen <henri@remonen.fi>
2023-10-28 11:07:04 +0300

    feat(Config): Update the config write function

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -74,8 +74,10 @@ func ReadConfigFile() (*Config, error) {
 
 // WriteConfigFile writes the configuration file to the user's home directory.
 func WriteConfigFile(config *Config) error {
-	viper.SetConfigFile(path.Join(UserHomeDir, configFile))
+	viper.AddConfigPath(UserHomeDir)
 	viper.SetConfigType("yaml")
+	viper.SetConfigName(configFile)
+	
 	viper.Set("commit_types", config.CommitTypes)
 	viper.Set("skip_ci_types", config.SkipCITypes)
 
